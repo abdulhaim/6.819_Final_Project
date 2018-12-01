659079eb80324a2c7cfce76e857982b07b137c1c
Marwa Abdulhai <marwaabdulhai@hotmail.com>
2018-11-30 23:57:24 -0500

    working download images

--- cleaning_data/download_images.py
+++ cleaning_data/download_images.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import urllib
 import os
+import pickle
 
 df = pd.read_csv("Batch_214780_batch_results-1.csv")
 
@@ -14,14 +15,21 @@ df.loc[df['Answer.Tag2'] == 5, 'Answer.Tag2'] = "moderate"
 df.loc[df['Answer.Tag2'] == 6, 'Answer.Tag2'] = "minimal/none"
 df.loc[df['Answer.Tag2'] == 7, 'Answer.Tag2'] =  "n/a"
 
+pickle_file = []
+if not os.path.exists("images"):
+        os.makedirs("images")
 for index, row in df.iterrows():
-    directory = row["Answer.Tag2"] 
+    directory = "images/" + row["Answer.Tag2"] 
     if not os.path.exists(directory):
         os.makedirs(directory)
     url = row['Input.image_url']
     name = directory + '/' +  row["AssignmentId"] + '.jpg'
+    pickle_file.append([name,row["Answer.Tag2"]])
     try:
-	urllib.request.urlretrieve(url, name)
-    except urllib.HTTPError:
-	continue
+        urllib.request.urlretrieve(url, name)
+    except urllib.error.HTTPError:
+        continue
+
+with open("pickle_file", 'wb') as handle:
+        pickle.dump(pickle_file, handle, protocol=2)
     
