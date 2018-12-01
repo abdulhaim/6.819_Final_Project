a861327e95a223479482064bd3c194d71078e204
Marwa Abdulhai <marwaabdulhai@hotmail.com>
2018-12-01 15:00:29 -0500

    combining dattasets

--- cleaning_data/download_images.py
+++ cleaning_data/download_images.py
@@ -4,20 +4,37 @@ import urllib
 import os
 import pickle
 
-#df = pd.read_csv("Batch_214780_batch_results-1.csv")
-df = pd.read_csv("three_3002.csv")
-#df = df.drop(['AutoApprovalDelayInSeconds','Expiration','NumberOfSimilarHITs','LifetimeInSeconds','WorkerId','AssignmentStatus','AcceptTime','SubmitTime','AutoApprovalTime','ApprovalTime','RejectionTime','RequesterFeedback','LifetimeApprovalRate','HITId','HITTypeId','Description','Reward', 'CreationTime','Last30DaysApprovalRate','MaxAssignments','Answer.Tag1','WorkTimeInSeconds','Title','Keywords','AssignmentDurationInSeconds','RequesterAnnotation','Last7DaysApprovalRate','Approve','Reject'], axis=1)
-df = df.drop(['AutoApprovalDelayInSeconds','Expiration','NumberOfSimilarHITs','LifetimeInSeconds','WorkerId','AssignmentStatus','AcceptTime','SubmitTime','AutoApprovalTime','ApprovalTime','RejectionTime','RequesterFeedback','LifetimeApprovalRate','HITId','HITTypeId','Description','Reward', 'CreationTime','Last30DaysApprovalRate','MaxAssignments','WorkTimeInSeconds','Title','Keywords','AssignmentDurationInSeconds','RequesterAnnotation','Last7DaysApprovalRate','Approve','Reject'], axis=1)
-#df.loc[df['Answer.Tag2'] == 4, 'Answer.Tag2'] = "extreme" 
-#df.loc[df['Answer.Tag2'] == 5, 'Answer.Tag2'] = "moderate" 
-#df.loc[df['Answer.Tag2'] == 6, 'Answer.Tag2'] = "minimal/none"
-#df.loc[df['Answer.Tag2'] == 7, 'Answer.Tag2'] =  "none"
-df.loc[df["Answer.Tag2"] == "n/a","Answer.Tag2"] = "none"
+# Combining csv's 
+
+df_1 = pd.read_csv("Batch_214780_batch_results-1.csv")
+df_1 = df_1.drop(['AutoApprovalDelayInSeconds','Expiration','NumberOfSimilarHITs','LifetimeInSeconds','WorkerId','AssignmentStatus','AcceptTime','SubmitTime','AutoApprovalTime','ApprovalTime','RejectionTime','RequesterFeedback','LifetimeApprovalRate','HITId','HITTypeId','Description','Reward', 'CreationTime','Last30DaysApprovalRate','MaxAssignments','Answer.Tag1','WorkTimeInSeconds','Title','Keywords','AssignmentDurationInSeconds','RequesterAnnotation','Last7DaysApprovalRate','Approve','Reject'], axis=1)
+
+df_1.loc[df_1['Answer.Tag2'] == 4, 'Answer.Tag2'] = "extreme" 
+df_1.loc[df_1['Answer.Tag2'] == 5, 'Answer.Tag2'] = "moderate" 
+df_1.loc[df_1['Answer.Tag2'] == 6, 'Answer.Tag2'] = "minimal"
+df_1.loc[df_1['Answer.Tag2'] == 7, 'Answer.Tag2'] =  "not applicable"
+
 pickle_file = []
 if not os.path.exists("images"):
         os.makedirs("images")
-for index, row in df.iterrows():
-    print(row["Answer.Tag2"])
+
+df_2 = pd.read_csv("three_3002.csv")
+df_2 = df_2.drop(['AutoApprovalDelayInSeconds','Expiration','NumberOfSimilarHITs','LifetimeInSeconds','WorkerId','AssignmentStatus','AcceptTime','SubmitTime','AutoApprovalTime','ApprovalTime','RejectionTime','RequesterFeedback','LifetimeApprovalRate','HITId','HITTypeId','Description','Reward', 'CreationTime','Last30DaysApprovalRate','MaxAssignments','WorkTimeInSeconds','Title','Keywords','AssignmentDurationInSeconds','RequesterAnnotation','Last7DaysApprovalRate','Approve','Reject'], axis=1)
+df_2.fillna("not applicable", inplace=True)
+
+for index, row in df_1.iterrows():
+    directory = "images/" + row["Answer.Tag2"] 
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    url = row['Input.image_url']
+    name = directory + '/' +  row["AssignmentId"] + '.jpg'
+    pickle_file.append([name,row["Answer.Tag2"]])
+    try:
+        urllib.request.urlretrieve(url, name)
+    except urllib.error.HTTPError:
+        continue
+
+for index, row in df_2.iterrows():
     directory = "images/" + row["Answer.Tag2"] 
     if not os.path.exists(directory):
         os.makedirs(directory)
